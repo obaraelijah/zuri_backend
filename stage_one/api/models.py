39992c7ff943ad77b69b08b43d65f90f366ccddf
obaraelijah <elijahobara357@gmail.com>
2023-09-08 09:57:24 +0300

    feat:  add api endpoints and basic configs

--- stage_one/api/models.py
+++ stage_one/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    slack_name = models.CharField(max_length=100)
+    utc_now = models.DateTimeField()
+    track = models.CharField(max_length=100)
+    github_file_url = models.URLField()
+    github_repo_url = models.URLField()
--- stage_one/api/views.py
+++ stage_one/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class RetrieveApi(APIView):
+    def get(self, request):
+        slack_name = request.GET.get("slack_name")
+        track = request.GET.get("track")
+
+        data = {
+            "slack_name": slack_name,
+            "current_day": datetime.now().strftime('%A'),
+            "utc_time": (datetime.utcnow() + timedelta(hours=0)).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "track": track,
+            "github_file_url": "https://github.com/obaraelijah/zuri_backend/blob/main/api/views.py",
+            "github_repo_url": "https://github.com/obaraelijah/zuri_backend/tree/main",
+            "status_code": 200
+        }
+        return Response(data)
